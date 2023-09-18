605bafbcf3c76efd8ba6cc9a72a7fa7a5ee7fd8f
yahyacodes <yahya.codin@gmail.com>
2023-09-18 14:29:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Yahya Dahir";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, YAHYA DAHIR!`;
+
+const shortGreeting = `Welcome, Yahya!`;
